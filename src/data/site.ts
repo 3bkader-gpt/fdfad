9fa54b4184f9d525506970be916231f5c69d1011
mohamed omar <medo.omar.salama@gmail.com>
2026-06-01 14:28:12 +0300

    chore: update primary whatsapp contact number to 01023100767

--- src/data/site.ts
+++ src/data/site.ts
@@ -1,4 +1,4 @@
-export const WHATSAPP_URL = 'https://wa.me/201062157349';
+export const WHATSAPP_URL = 'https://wa.me/201023100767';
 export const CONTACT_EMAIL = 'hello@fadfaad.com';
 export const INSTAGRAM_URL = 'https://www.instagram.com/shimaasamerfarouk';
 export const FACEBOOK_URL = 'https://www.facebook.com/share/1EQcMuQijU/';
